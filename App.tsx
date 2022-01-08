92bbfcbffba4fd2c0142b00d3ad8fb6c24d7047f
Frits Kastelein <frits@solutions4delivery.com>
2022-01-08 23:03:33 +0100

    Added 'react-navigation'
    package.json:
    yarn.lock:
     - added packages for 'react-navigation'
    
    android/*:
    ios/*:
     - setup for 'react-navigation' and related packages
    
    App.tsx:
     - added temp tabs

--- App.tsx
+++ App.tsx
@@ -1,30 +1,69 @@
 import React, { useEffect } from 'react';
-import { Button, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { View, Button, SafeAreaView, StatusBar, Text } from 'react-native';
 import { ActionTypes } from './redux/actionCreators';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { useTypedDispatch, useTypedSelector } from './redux/hooks'
 
-const App = () => {
-    const hoi = useTypedSelector();
+function HomeScreen() {
+        const hoi = useTypedSelector();
     const dispatch = useTypedDispatch()
 
-    useEffect(() => {
-        console.info('hoi=', hoi)
-    },[hoi])
+    return (
+              <SafeAreaView >
+        <StatusBar barStyle={'dark-content'} />
+        <Text>Hallo</Text>
+        <Button title='Random' onPress={() => {
+              dispatch({type: ActionTypes.COUNTER_CHANGE,
+              payload: Math.random()})
+        }} />
+  <Button title='RESET' onPress={() => {
+              dispatch({type: ActionTypes.COUNTER_RESET})
+        }} />
+      </SafeAreaView>
+    );
+  }
 
-  return (
-    <SafeAreaView >
-      <StatusBar barStyle={'dark-content'} />
-      <Text>Hallo</Text>
-      <Button title='Random' onPress={() => {
-            dispatch({type: ActionTypes.COUNTER_CHANGE,
-            payload: Math.random()})
-      }} />
-<Button title='RESET' onPress={() => {
-            dispatch({type: ActionTypes.COUNTER_RESET})
-      }} />
-    </SafeAreaView>
-  );
-};
-
-export default App;
+  function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+  
+  
+  const Tab = createBottomTabNavigator();
+  
+  function App() {
+    return (
+        <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+  
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'ios-information-circle'
+                  : 'ios-information-circle-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'ios-list-box' : 'ios-list';
+              }
+  
+              // You can return any component that you like here!
+              return <View style={{backgroundColor: color, width: size, height: size}}/>
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  export default App;
